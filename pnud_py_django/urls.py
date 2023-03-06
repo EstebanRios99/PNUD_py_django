f234ed87ce59a0d2fb01c4f17f95cbd6181f06db
Alex O <desarrollo4@pcnub.com>
2023-03-06 18:03:24 -0500

    Vista Base Redireccion Create New - Cambio Relacion Noticias

--- pnud_py_django/urls.py
+++ pnud_py_django/urls.py
@@ -21,5 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_App.urls')),
-    path('blog/', include('blog_App.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
