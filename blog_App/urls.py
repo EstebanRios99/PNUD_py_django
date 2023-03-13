b38dd58205d8670df8465bea31fee558b87a41e7
EstebanRios99 <esteban.rios@epn.edu.ec>
2023-03-12 23:01:51 -0500

    Implementación Api Framework y validaciones Implementación de vistas para cambio de contraseña Protección de rutas solo cuando exista un login Instalación de librería Django REST framework Implementación de rutas de api Api para consultar usuarios y noticias

--- blog_App/urls.py
+++ blog_App/urls.py
@@ -1,9 +1,14 @@
-from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.urls import path, include
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+from rest_framework import routers
+from blog_App.views import UserViewSet, NewViewSet, CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('news', NewViewSet)
+router.register('category-detail', CategoryViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -12,11 +17,17 @@ urlpatterns = [
     path('news', views.newsView, name='newsView'),
     path('news/category/<int:category_id>/', views.newsView_filter, name="category"),
     path('news/newsitem/<int:news_id>/', views.newsView_item, name="newsitem"),
-    path('create/new', views.createNew, name='create_new'),
-    path('update/new/<int:news_id>', views.updateNew, name='update_new'),
-    path('delete/new/<int:news_id>', views.deleteNew, name='delete_new'),
+    path('create/new', login_required(views.createNew), name='create_new'),
+    path('update/new/<int:news_id>', login_required(views.updateNew), name='update_new'),
+    path('delete/new/<int:news_id>', login_required(views.deleteNew), name='delete_new'),
     # Usuarios
     path('login', LoginView.as_view(),name='login'),
     path('logout', LogoutView.as_view(),name='logout'),
-    path('register', views.RegisterView.as_view(), name='register')
+    path('register', views.RegisterView.as_view(), name='register'),
+    path('change_password', views.MyPasswordChangeView.as_view(), name='password_change'),
+    path('change_password/done', views.MyPasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # Api
+    path('api/', include(router.urls))
+
 ]
