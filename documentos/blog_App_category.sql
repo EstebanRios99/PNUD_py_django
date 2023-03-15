632b3407330c55600eccb74e5d3f0a077bfcf919
unknown <desarrollo9@pcnub.com>
2023-03-15 14:01:35 -0500

    Archivos sql y modificaci{on Readme

---
+++ documentos/blog_App_category.sql
@@ -0,0 +1,8 @@
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (1, 'Nacionales', '2023-03-15 11:24:58.087295-05', '2023-03-15 11:24:58.087295-05');
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (2, 'Deportes', '2023-03-15 11:25:45.453134-05', '2023-03-15 11:25:45.453134-05');
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (3, 'Internacionales', '2023-03-15 11:26:19.458407-05', '2023-03-15 11:26:19.458407-05');
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (4, 'Ciencia y Tecnología', '2023-03-15 11:26:34.957654-05', '2023-03-15 11:26:34.957654-05');
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (5, 'Cultura y Cine', '2023-03-15 11:26:41.301424-05', '2023-03-15 11:26:41.301424-05');
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (6, 'Medio Ambiente', '2023-03-15 11:26:48.285869-05', '2023-03-15 11:26:48.285869-05');
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (7, 'Tendencias', '2023-03-15 11:27:30.799695-05', '2023-03-15 11:27:30.799695-05');
+INSERT INTO "blog_App_category" ("id", "name", "created", "updated") VALUES (8, 'Educación', '2023-03-15 11:28:03.699683-05', '2023-03-15 11:28:03.699683-05');
